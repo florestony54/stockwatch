f973e9a1f003c243bcb86fcb84050a04481e470a
Tony Flores <musclecard54@gmail.com>
2021-04-20 03:03:30 -0500

    tabs for popPanel, added market indexes

--- src/components/PopPanel.js
+++ src/components/PopPanel.js
@@ -14,6 +14,7 @@ class PopPanel extends React.Component{
         this.getPrices = () => {
             var url = new URL("http://localhost:5000/pop");
             fetch(url).then(response =>
+                // console.log(response.json())
                 response.json()
             ).then(dat =>
                 this.setState({
@@ -22,6 +23,11 @@ class PopPanel extends React.Component{
                     amzn: "$" + dat.amzn,
                     msft: "$" + dat.msft,
                     fb: "$" + dat.fb,
+                    sp500: dat.sp500,
+                    dow: dat.dow,
+                    nasdaq: dat.nasdaq,
+                    russel: dat.russel,
+                    vix: dat.vix,
                 })
             ).catch((err) => {
                 console.log(err);
@@ -42,44 +48,85 @@ class PopPanel extends React.Component{
 
     render(){
         return (
-            <div id='pop-stocks' className="card col-2" >
-                <div class="card-header">
-                    Popular Tech Stocks
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        <div className='row align-items-center'>
-                            <i class="fab fa-google col-1"></i>
-                            <div className='col-8'>GOOG <div className='pop-price'>{this.state.goog}</div></div>
-                        </div>
+            <div id='pop-stocks' className="card col-2 nav nav-tabs" >
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="tech-tab" data-toggle="tab" href="#tech" role="tab" aria-controls="tech" aria-selected="true">Tech</a>
                     </li>
-                    <li class="list-group-item">
-                        <div className='row align-items-center'>
-                            <i class="fab fa-apple col-1"></i>
-                            <div className='col-8'>AAPL <div className='pop-price'>{this.state.aapl}</div></div>
-                        </div>
-                        
-                    </li><li class="list-group-item">
-                        <div className='row align-items-center'>
-                            <i class="fab fa-amazon col-1"></i>
-                            <div className='col-8'>AMZN <div className='pop-price'>{this.state.amzn}</div></div>
-                        </div>
-                        
-                    </li><li class="list-group-item">
-                        <div className='row align-items-center'>
-                            <i class="fab fa-microsoft col-1"></i>
-                            <div className='col-8'>MSFT <div className='pop-price'>{this.state.msft}</div></div>
-                        </div>
-                        
-                    </li><li class="list-group-item">
-                        <div className='row align-items-center'>
-                            <i class="fab fa-facebook col-1"></i>
-                            <div className='col-8'>FB <div className='pop-price'>{this.state.fb}</div></div>
-                        </div>
-                        
+                    <li class="nav-item">
+                        <a class="nav-link" id="market-tab" data-toggle="tab" href="#market" role="tab" aria-controls="market" aria-selected="false">US Market</a>
                     </li>
-
                 </ul>
+                {/* <div class="card-header">
+                    Popular Tech Stocks
+                </div> */}
+                <div className='tab-content' id='myTabcontent'>
+                    {/* Tech */}
+                    <div id='tech' class="tab-pane fade show active list-group list-group-flush " role="tabpanel" aria-labelledby="tech-tab">
+                        <li class="list-group-item">
+                            <div className='row align-items-center'>
+                                <i class="fab fa-google col-1"></i>
+                                <div className='col-8'>GOOG <div className='pop-price'>{this.state.goog}</div></div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div className='row align-items-center'>
+                                <i class="fab fa-apple col-1"></i>
+                                <div className='col-8'>AAPL <div className='pop-price'>{this.state.aapl}</div></div>
+                            </div>
+                            
+                        </li><li class="list-group-item">
+                            <div className='row align-items-center'>
+                                <i class="fab fa-amazon col-1"></i>
+                                <div className='col-8'>AMZN <div className='pop-price'>{this.state.amzn}</div></div>
+                            </div>
+                            
+                        </li><li class="list-group-item">
+                            <div className='row align-items-center'>
+                                <i class="fab fa-microsoft col-1"></i>
+                                <div className='col-8'>MSFT <div className='pop-price'>{this.state.msft}</div></div>
+                            </div>
+                            
+                        </li><li class="list-group-item">
+                            <div className='row align-items-center'>
+                                <i class="fab fa-facebook col-1"></i>
+                                <div className='col-8'>FB <div className='pop-price'>{this.state.fb}</div></div>
+                            </div>
+                            
+                        </li>
+
+                    </div>
+                    {/* Market */}
+                    <div id='market' class="tab-pane fade list-group list-group-flush " role="tabpanel" aria-labelledby="market-tab">
+                        <li class="list-group-item">
+                            <div className='row align-items-center'>
+                                <div className='col-8'>Dow<div className='pop-price'>{this.state.dow }</div></div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div className='row align-items-center'>
+                                <div className='col-8'>S&P 500<div className='pop-price'>{ this.state.sp500}</div></div>
+                            </div>
+
+                        </li><li class="list-group-item">
+                            <div className='row align-items-center'>
+                                <div className='col-8'>Nasdaq <div className='pop-price'>{this.state.nasdaq }</div></div>
+                            </div>
+
+                        </li><li class="list-group-item">
+                            <div className='row align-items-center'>
+                                <div className='col-8'>Russel <div className='pop-price'>{this.state.russel }</div></div>
+                            </div>
+
+                        </li><li class="list-group-item">
+                            <div className='row align-items-center'>
+                                <div className='col-8'>VIX <div className='pop-price'>{this.state.vix }</div></div>
+                            </div>
+
+                        </li>
+
+                    </div>
+                </div>
             </div>
             )
     }
